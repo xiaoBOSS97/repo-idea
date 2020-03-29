75f861995acb453ba198060f42386ea41ed1b656
JiachenDai <826500825@qq.com>
2020-03-29 15:43:22 +0800

    提交idea中的gitproject

---
+++ gitTest/src/com/djc/git/FirstClass.java
@@ -0,0 +1,14 @@
+package com.djc.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+
+
+    }
+}
